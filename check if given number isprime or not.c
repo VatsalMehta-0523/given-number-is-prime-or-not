87056e1e148d0c1624d75adaefe4996dff7f0e12
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-04 00:55:34 +0530

    Add files via upload

---
+++ check if given number isprime or not.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n , cnt = 0;
+    printf("enter a value : ");
+    scanf("%d",&n);
+
+    printf("factors of %d are :  ", n );
+    for (int i = 1 ; i <= n ; i++){
+        if ( n % i == 0 ){
+            printf("%d ",i);
+            cnt++;
+        }
+    }
+
+    if (cnt == 2){
+        printf("\n%d is a prime number ",n);
+    }
+    else{
+        printf("\n%d is not a prime number ",n);
+    }
+    return 0 ;
+}
